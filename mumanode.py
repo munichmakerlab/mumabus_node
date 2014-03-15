70455571f8de0f0c60a6d232e34593f80d0c4d8a
Severin Schols <severin@schols.de>
2014-03-15 18:27:14 +0100

    use paho mqtt client

--- mumanode.py
+++ mumanode.py
@@ -1,23 +1,60 @@
-import mosquitto
+import paho.mqtt.client as paho
 from mumadevices import *
+from threading import Timer
+import logging
 
 import config
 
+
+def on_connect(mosq, obj, rc):
+	logging.info("Connect with RC " + str(rc))
+	mosq.subscribe(config.topic, 0)
+
 def on_message(mosq, obj, msg):
+	logging.info(msg.topic + " [" + str(msg.qos) + "]: " + str(msg.payload))
 	# remove prefix from topic.
 	if msg.topic.startswith(config.topic_prefix):
 		node = msg.topic[len(config.topic_prefix):]
 	else:
 		return
 	node, topic = node.split('/', 1)
-	print "[MSG] " +  node + ": " + topic + ";" + msg.payload
 	items[node].action(topic, msg.payload)
 
-# initialize MQTT
-mqtt_client = mosquitto.Mosquitto(config.node_name)
-mqtt_client.username_pw_set(config.broker["user"], config.broker["password"])
-mqtt_client.on_message = on_message
-mqtt_client.connect(config.broker["hostname"], config.broker["port"], 60)
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+	logging.info("Subscribed: "+str(mid)+" "+str(granted_qos))
+
+def on_disconnect(client, userdata, rc):
+	logging.warning("Disconnected (RC " + str(rc) + ")")
+	if rc <> 0:
+		try_reconnect(client)
+
+def on_log(client, userdata, level, buf):
+	logging.debug(buf)
+	
+def try_reconnect(client, time = 60):
+	try:
+		logging.info("Trying reconnect")
+		client.reconnect()
+	except:
+		logging.warning("Reconnect failed. Trying again in " + str(time) + " seconds")
+		Timer(time, try_reconnect, [client]).start()
+
+logging.basicConfig(format='[%(levelname)s] %(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
+
+logging.info("Initializing MQTT")
+mqttc = paho.Client(config.node_name)
+mqttc.username_pw_set(config.broker["user"], config.broker["password"])
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_disconnect = on_disconnect
+mqttc.on_subscribe = on_subscribe
+mqttc.on_log = on_log
+try:
+	mqttc.connect(config.broker["hostname"], config.broker["port"])
+except:
+	logging.warning("Connection failed. Trying again in 30 seconds")
+	Timer(30, try_reconnect, [mqttc]).start()
 
 items = {}
 
@@ -25,7 +62,7 @@ for (name, details) in config.devices.items():
 	item = {}
 	if details["type"] == "switch":
 		if len(details["pins"]) != 1:
-			print "Device \"" + name + "\" is invalid."
+			logging.error("Device \"" + name + "\" is invalid.")
 			continue
 		item = SwitchDevice(name, board, details["pins"][0])
 		mqtt_client.subscribe(config.topic_prefix + name + "/#", 1)
@@ -33,6 +70,19 @@ for (name, details) in config.devices.items():
 		item = RCSwitchDevice(name, details["address"])
 		mqtt_client.subscribe(config.topic_prefix + name + "/#", 1)
 	items[name] = (item)
-	print "Added device " + name
+	logging.info("Added device " + name)
+
+logging.info("Entering loop")
+try:
+	mqttc.loop_forever()
+except KeyboardInterrupt:
+	pass
+
+logging.info("Exiting")
+mqttc.disconnect()
+
+
+
+
 
 mqtt_client.loop_forever()
